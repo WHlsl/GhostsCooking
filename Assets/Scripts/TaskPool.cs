ed91a704168385f1c47d5953ed8d7992a369d8db
WHlsl <1291861473@qq.com>
2017-06-28 21:06:27 +0800

    2017.06.28

---
+++ Assets/Scripts/TaskPool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskPool : MonoBehaviour {
+
+    private GameObject[] tasks;
+    public GameObject taskPrefab;
+    public GameObject horizontalParent;
+    public int needCount = 3;
+    public int MaxTaskCount = 5;
+    private int needGreenCount;
+    private int needYellowCount;
+    public void NewTask()
+    {
+        int i = 0;
+        needGreenCount = Random.Range(0, 3);
+        needYellowCount = needCount - needGreenCount;
+        if (i < MaxTaskCount&&)
+        {
+            tasks[i]=Instantiate(taskPrefab);
+            tasks[i].transform.SetParent(horizontalParent.transform);
+            i++;
+        }
+        
+        
+        
+    }
+}
